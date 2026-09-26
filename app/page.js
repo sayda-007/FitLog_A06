1c62d4d5aa607eb1489cb80d43b8684ae0e2ef7a
Sayda <saayda007@gmail.com>
2026-09-26 20:31:18 +0600

    add FitLog API data fetching

--- app/page.js
+++ app/page.js
@@ -1,7 +1,17 @@
-export default function Home() {
+import { getWorkouts } from "../lib/api";
+
+export default async function Home() {
+  const workouts = await getWorkouts();
+
   return (
     <main>
       <h1>FitLog</h1>
+
+      <ul>
+        {workouts.map((workout) => (
+          <li key={workout.id}>{workout.name}</li>
+        ))}
+      </ul>
     </main>
   );
 }
